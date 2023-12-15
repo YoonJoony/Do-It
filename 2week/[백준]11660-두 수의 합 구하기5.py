da0a38470b909b80ffdd0767ff72450b1b85c8df
YoonJoony <dbswns1101@naver.com>
2023-12-16 02:06:53 +0900

    누적 합 배열 사용한 코드

--- 2week/[백준]11660-두 수의 합 구하기5.py
+++ 2week/[백준]11660-두 수의 합 구하기5.py
@@ -1,20 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-
-array = []
-array_sum = 0
-
+N, M = map(int, input().split())
+A = []
 for i in range(N):
-    array.append(list(map(int, input().split())))
+    A.append(list(map(int, input().split())))
 
-for i in range(K):
-    x1, y1, x2, y2 = map(int, input().split())
+D = [[0]*(N+1) for _ in range(N+1)]
 
-    for j in range(x2-x1+1):
-        for k in range(y1-1, y2):
-            array_sum += array[x1-1+j][k]
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i-1][j-1]
 
-    print(array_sum)
-    array_sum = 0
+for i in range(M):
+    X1, Y1, X2, Y2 = map(int, input().split())
+
+    result = D[X2][Y2] - D[X1-1][Y2] - D[X2][Y1-1] + D[X1-1][Y1-1]
+    print(result)
