da4c5be7e0f4fbc3399d722e616188103f6b50d9
YoonJoony <dbswns1101@naver.com>
2023-12-16 20:33:38 +0900

    2차 작성 코드 (while) for문 삭제 후 while만 반복시킴 결과 : 시간초과

--- 2week/[백준]10986-나머지 합.py
+++ 2week/[백준]10986-나머지 합.py
@@ -1,3 +1,7 @@
+import sys
+
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
 loop_N = N
@@ -5,16 +9,26 @@ loop_N = N
 A = list(map(int, input().split()))
 flag = 0
 count = 0
+i = 1
+D = [0 for _ in range(N + 1)]
+
 while flag < loop_N:
-    D = [0 for _ in range(N+1)]
+    D[i] = D[i - 1] + A[i + flag - 1]
 
-    for i in range(1, len(D)):
-        D[i] = D[i-1] + A[i+flag-1]
+    if D[i] % M == 0:
+        print(D[i])
+        count += 1
 
-        if D[i] % M == 0:
-            count += 1
-
-    N -= 1
-    flag += 1
+    if i == N:
+        flag += 1
+        i = 1
+        N -= 1
+        D = [0 for _ in range(N + 1)]
+    else:
+        i += 1
 
 print(count)
+
+
+
+
