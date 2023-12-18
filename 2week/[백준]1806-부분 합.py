68993ca55047437f83a39b57ec3ff95861680138
YoonJoony <dbswns1101@naver.com>
2023-12-19 01:40:52 +0900

    2차 작성 코드 구간 합 x 투 포인터로 누적 합 계산. 결과 : 성공

--- 2week/[백준]1806-부분 합.py
+++ 2week/[백준]1806-부분 합.py
@@ -5,20 +5,19 @@ N, S = map(int, input().split())
 
 A = list(map(int, input().split()))
 end = 0
-count = []
 sum = 0
+count = 0
 for i in range(N):
     while sum < S and end < N:
         sum += A[end]
         end += 1
     if sum >= S:
-        # for j in range(i, end):
-        #     print(f"{A[j]}", end=" ")
-        # print()
-        count.append(end-i)
+        if count == 0:
+            count = end-i
+        else:
+            count = min(count, (end-i))
     sum -= A[i]
+
 print(count)
-
-
 
 
