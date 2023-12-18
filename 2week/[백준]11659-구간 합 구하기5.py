5c8806fdb74322f7b755184e994bb6e96b3a1df6
YoonJoony <dbswns1101@naver.com>
2023-12-18 21:09:58 +0900

    2차 작성 코드 구간 합 x(실패유도). 결과 : 시간초과

--- 2week/[백준]11659-구간 합 구하기5.py
+++ 2week/[백준]11659-구간 합 구하기5.py
@@ -4,12 +4,29 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 A = list(map(int, input().split()))
-D = [0] * (N+1)
-
-for i in range(1, N+1):
-    D[i] = D[i-1] + A[i-1]
-
+S = 0
 for i in range(M):
     num1, num2 = map(int, input().split())
-    S = D[num2] - D[num1-1]
+
+    for i in range((num2-num1)+1):
+        S += A[i+(num1-1)]
+
     print(S)
+    S = 0
+
+# import sys
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().split())
+#
+# A = list(map(int, input().split()))
+# D = [0] * (N+1)
+#
+# for i in range(1, N+1):
+#     D[i] = D[i-1] + A[i-1]
+#
+# for i in range(M):
+#     num1, num2 = map(int, input().split())
+#     S = D[num2] - D[num1-1]
+#     print(S)
+
