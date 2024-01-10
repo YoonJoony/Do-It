09ab2fa066b4d57301590c00c5550ff2f9eb799e
YoonJoony <dbswns1101@naver.com>
2024-01-11 01:02:58 +0900

    2차 코딩 - 성공 보고함. 무방향 그래프인 경우 graph[x][y]만 성정해줄 것이 아니라 graph[y][x]도 설정 해줘야함

--- 5week/[백준]1260-DFS와 BFS.py
+++ 5week/[백준]1260-DFS와 BFS.py
@@ -1,64 +1,52 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
 N, M, V = map(int, input().split())
 
-graph = [[] * N for _ in range(N + 1)]
+graph = [[] * (N + 1) for _ in range(N + 1)]
 
 for i in range(1, M + 1):
     n1, n2 = map(int, input().split())
     graph[n1].append(n2)
-    graph[n1].sort()
+    graph[n2].append(n1)
 
-visited = [False] * (N+1)
+for i in range(1, N + 1):
+    graph[i].sort()
 
+visited = [False] * (N + 1)
+
+print(graph)
 # DFS 구현
 def dfs(graph, v, visited):
     visited[v] = True
     print(v, end=" ")
-    if not graph[v]:
-        v2 = v
-        for i in range(1, N+1):
-            if graph[i].count(v) != 0 and not visited[i]:
-                v2 = i
 
-        if not visited[v2]:
-            visited[v2] = True
-            print(v2, end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-        for i in graph[v2]:
-            if not visited[i]:
-                dfs(graph, i, visited)
-    else:
-        for i in graph[v]:
-            if not visited[i]:
-                dfs(graph, i, visited)
 
 dfs(graph, V, visited)
 
-print("\n", end="")
+print()
 
 # BFS 구현
 def bfs(graph, start, visited):
-    visited = [False] * (N+1)
-
+    global queue
     visited[start] = True
-    queue = deque([start])
 
     while queue:
         v = queue.popleft()
         print(v, end=" ")
+
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
 
-        if not graph[v]:
-            for i in range(1, N+1):
-                if graph[i].count(v) != 0 and not visited[i]:
-                    visited[i] = True
-                    queue.append(i)
 
-
+queue = deque([V])
+visited = [False] * (N + 1)
 bfs(graph, V, visited)
