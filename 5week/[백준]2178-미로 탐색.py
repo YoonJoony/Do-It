40ab040faf360d54dfd1c0521997fb134f121e12
YoonJoony <dbswns1101@naver.com>
2024-01-11 06:45:45 +0900

    1차 코딩 - 틀림 답은 잘 나오는데 문제 상 bfs만 풀게 되어서 재귀되면 틀리게 만들어진듯

--- 5week/[백준]2178-미로 탐색.py
+++ 5week/[백준]2178-미로 탐색.py
@@ -11,7 +11,7 @@ visited = [[False for _ in range(M)] for _ in range(N)]
 dx = [1, -1]
 dy = [1, -1]
 result = []
-count = 1
+count = 1 # 거리 카운트
 
 
 def dfs(y, x, graph):
@@ -27,12 +27,14 @@ def dfs(y, x, graph):
             mx = x + dx[i] # 이동시킬 좌표
             my = y + dy[i]
 
+            # 마지막 좌표가 아니면서 이동할 좌표가 범위를 벗어나지 않으면서 노드를 방문 안헀을 시
             if (y != N - 1 or x != M - 1) and 0 <= mx < M and graph[y][mx] == 1 and not visited[y][mx]:
                 count += 1
                 dfs(y, mx, graph)
             if (y != N - 1 or x != M - 1) and 0 <= my < N and graph[my][x] == 1 and not visited[my][x]:
                 count += 1
                 dfs(my, x, graph)
+    # 위 재귀함수 호출 후 재귀될 때 거리를 다시 원상 복구하여 1씩 줄인다.
     count -= 1
 
 
