0677895577a03fb5952cf66bbbc3633b06b78fb0
YoonJoony <dbswns1101@naver.com>
2024-01-11 04:32:44 +0900

    1차 코딩 - 성공 1시간 걸림. 알고리즘은 잘 짰는데 실수한 코드 한 줄 못봐서 30분 더 걸림

--- 5week/[백준]1926-그림.py
+++ 5week/[백준]1926-그림.py
@@ -11,19 +11,20 @@ for _ in range(N):
 
 visited = [[False for _ in range(M)] for _ in range(N)]
 
-result = [0]
-count = 0
+result = [0] # 그림 카운트한 결과 저장 배열(그림 개수만큼)
+count = 0 # 그림 카운트 변수
 def dfs(y, x, graph):
     global visited, count
     visited[y][x] = True
     dx = [1, -1]
     dy = [1, -1]
-    if graph[y][x] == 1:
-        count += 1
+    if graph[y][x] == 1: # 현재 좌표의 값이 1일 경우
+        count += 1 # 그림 크기 카운트
         for i in range(2):
-            mx = x + dx[i]
-            my = y + dy[i]
+            mx = x + dx[i] # x좌표 좌우
+            my = y + dy[i] # y좌표 좌우
             if 0 <= mx < M and graph[y][mx] == 1 and not visited[y][mx]:
+                # 재귀 함수를 통해 좌표를 이동한 값이 1이 안될 때까지 재귀 및 반복
                 dfs(y, mx, graph)
             if 0 <= my < N and graph[my][x] == 1 and not visited[my][x]:
                 dfs(my, x, graph)
