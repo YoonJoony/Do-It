734b83c308803e63ece4638f901a0f725d008ed1
YoonJoony <dbswns1101@naver.com>
2024-01-14 18:45:10 +0900

    4차 수정 -> 시간초과 시간초과 이유: num.remove(num[0]) 함수는 O(n) 시간복잡도를 가지므로 매우 큰 입력 값에 대해 시간 초과가 발생할 수 있다.
    따라서 효율적인 입력에 대해 덱(deque)를 활용해야된다.

--- 4week/[백준]5430-AC.py
+++ 4week/[백준]5430-AC.py
@@ -1,42 +1,34 @@
 import sys
+
 import re
 input = sys.stdin.readline
 
 for _ in range(int(input())):
     p = input().rstrip('\n')
-    st = []
-    delete_count = 1
     n = int(input())
     num = re.findall(r'\d+', input().rstrip('\n'))
 
+    reverse_flag = True
+    error_flag = True
+
     for val in p:
         if val == "R":
-            delete_count = 1
-            if st and st[-1] == "R":
-                st.pop()
+            if reverse_flag:
+                reverse_flag = False
             else:
-                st.append(val)
-        elif val == "D":
-            if st and isinstance(st[-1], (int, float)):
-                st.pop()
-                delete_count += 1
-                st.append(delete_count)
-            else:
-                st.append(delete_count)
-
-    for val in st:
-        if val == "R":
-            num.reverse()
+                reverse_flag = True
         else:
-            if val > len(num):
-                num = "error"
+            if not num:
+                error_flag = False
+                print("error")
                 break
-            else:
-                del num[0:val]
+            if reverse_flag:
+                num.remove(num[0])
+            elif not reverse_flag:
+                num.remove(num[-1])
 
-    if num != "error":
-        print("[", end="")
-        print(",".join(num), end="")
-        print("]")
-    else:
-        print(num)
+    if not reverse_flag:
+        num.reverse()
+
+    if error_flag:
+        print('[' + ','.join(num) + ']')
