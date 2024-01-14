0898a1568521a332befe03990991f56545565fb7
YoonJoony <dbswns1101@naver.com>
2024-01-14 17:54:03 +0900

    2차 수정 -> 시간초과

--- 4week/[백준]5430-AC.py
+++ 4week/[백준]5430-AC.py
@@ -3,27 +3,26 @@ import re
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    cal = []
-    for val in input().rstrip('\n'):
-        cal.append(val)
-    N = int(input())
-    input_string = input().rstrip('\n')
-    arr = [int(num) for num in re.findall(r'\d+', input_string)]
-    re_arr = []
+    p = input()
+    n = int(input())
+    num = re.findall(r'\d+', input().rstrip('\n'))
 
-    # 여기가 문제? RDRRDRD 일 경우는 계산이 안됨.
-    if cal.count('R') % 2 == 1:
-        re_arr.extend(reversed(arr))
-        arr = re_arr
-
-    D_count = cal.count('D')
-
-    if len(arr) < D_count:
-        print("error")
+    for val in p:
+        if val == "R":
+            num.reverse()
+        elif val == "D":
+            if not num:
+                num = "error"
+                break
+            else:
+                num.remove(num[0])
+    if num != "error":
+        print("[", end="")
+        print(",".join(num), end="")
+        print("]")
     else:
-        result = arr[D_count:]
-        print(str(result).replace(" ", ""))
+        print(num)
 
 
 
-
+# 100 x 100000
