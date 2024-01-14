35bbf9a7ddd3a9d4433bd2613ec698ff6e3baf17
YoonJoony <dbswns1101@naver.com>
2024-01-14 18:25:30 +0900

    3차 수정 -> 시간초과

--- 4week/[백준]5430-AC.py
+++ 4week/[백준]5430-AC.py
@@ -3,26 +3,40 @@ import re
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    p = input()
+    p = input().rstrip('\n')
+    st = []
+    delete_count = 1
     n = int(input())
     num = re.findall(r'\d+', input().rstrip('\n'))
 
     for val in p:
         if val == "R":
-            num.reverse()
+            delete_count = 1
+            if st and st[-1] == "R":
+                st.pop()
+            else:
+                st.append(val)
         elif val == "D":
-            if not num:
+            if st and isinstance(st[-1], (int, float)):
+                st.pop()
+                delete_count += 1
+                st.append(delete_count)
+            else:
+                st.append(delete_count)
+
+    for val in st:
+        if val == "R":
+            num.reverse()
+        else:
+            if val > len(num):
                 num = "error"
                 break
             else:
-                num.remove(num[0])
+                del num[0:val]
+
     if num != "error":
         print("[", end="")
         print(",".join(num), end="")
         print("]")
     else:
         print(num)
-
-
-
-# 100 x 100000
