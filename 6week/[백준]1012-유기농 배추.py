dcf9de32cdc7eb9a7550c47479d43b2ca718238a
YoonJoony <dbswns1101@naver.com>
2024-01-18 02:32:44 +0900

    2차 수정 -> 성공 mx, my와 N, M의 매칭을 잘못 시켜줘서 안된거였음

--- 6week/[백준]1012-유기농 배추.py
+++ 6week/[백준]1012-유기농 배추.py
@@ -4,38 +4,38 @@ from collections import deque
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-N, M, K = map(int, input().split())
-graph = [[0 for c in range(N)] for v in range(M)]
-result = 0
-for _ in range(K):
-    n2, n1 = map(int, input().split())
-    graph[n1][n2] = 1
+for _ in range(int(input())):
+   N, M, K = map(int, input().split())
+   graph = [[0 for c in range(N)] for v in range(M)]
+   result = 0
+   for _ in range(K):
+       n2, n1 = map(int, input().split())
+       graph[n1][n2] = 1
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+   dx = [-1, 1, 0, 0]
+   dy = [0, 0, -1, 1]
 
-def bfs(graph, x, y):
-    queue = deque([(x, y)])
-    graph[x][y] = -1
+   def bfs(graph, x, y):
+       queue = deque([(x, y)])
+       graph[x][y] = -1
 
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            mx = x + dx[i]
-            my = y + dy[i]
+       while queue:
+           x, y = queue.popleft()
+           for i in range(4):
+               mx = x + dx[i]
+               my = y + dy[i]
 
-            if mx < 0 or mx >= N or my < 0 or my >= M:
-                continue
-            if graph[mx][my] == 1:
-                queue.append((mx, my))
-                graph[mx][my] = -1
+               if mx < 0 or mx >= M or my < 0 or my >= N:
+                   continue
+               if graph[mx][my] == 1:
+                   queue.append((mx, my))
+                   graph[mx][my] = -1
 
 
-for i in range(M):
-    for j in range(N):
-        if graph[i][j] == 1:
-            bfs(graph, i, j)
-            print(i, j)
-            result += 1
+   for i in range(M):
+       for j in range(N):
+           if graph[i][j] == 1:
+               bfs(graph, i, j)
+               result += 1
 
-print(result)
+   print(result)
