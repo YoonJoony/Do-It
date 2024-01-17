79dca66ce55d9f39bd3d48fda7c46859a2dba6bd
YoonJoony <dbswns1101@naver.com>
2024-01-18 04:10:18 +0900

    1차 작성 -> 성공 좀 이상한 문제.
    강수량을 0부터 가장 큰 건물-1 까지
    전부 계산 해줘야 한다는...
    
    성공하긴 했는데 시간이 좀 걸려서
    단축 시켜봐도 좋을 것 같다
    
    문제는 그냥 간단한 응용 bfs 문제

--- 6week/[백준]2468-안전영역.py
+++ 6week/[백준]2468-안전영역.py
@@ -4,14 +4,15 @@ from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
-precipitation = 0
+precipitation = 0  # 강수량 저장 변수
 graph = []
 
 safe_area = []
 
 for i in range(N):
     graph.append(list(map(int, input().split())))
-    precipitation = max(graph[i])
+    precipitation = max(graph[i])  # 가장 큰 건물 -1 만큼 강수량의 안전영역을 조사해야 하기 때문에
+    # 강수량 변수에 가장 큰 건물의 높이만큼 저장함.
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -19,7 +20,9 @@ dy = [0, 0, -1, 1]
 
 def bfs(graph, x, y, p):
     queue = deque([(x, y)])
-    visit_graph[x][y] = True
+    visit_graph[x][y] = True  # 현재 좌표의 값을 변경시켜주는 다른 BFS 문제와 달리 강수량 만큼 다시
+    # 그래프를 돌려야 하기 때문에 그래프를 매번 다시 만드는 것 보다
+    # 방문 여부 좌표를 하나 더 만드는게 낫다.
 
     while queue:
         x, y = queue.popleft()
@@ -28,21 +31,22 @@ def bfs(graph, x, y, p):
             my = y + dy[i]
             if mx < 0 or mx >= N or my < 0 or my >= N:
                 continue
-            if graph[mx][my] > p and not visit_graph[mx][my]:
+            if graph[mx][my] > p and not visit_graph[mx][my]:  # 안전 영역일 경우 조건
                 visit_graph[mx][my] = True
                 queue.append((mx, my))
 
 
-for p in range(precipitation):
-    safe_area_count = 0
+for p in range(precipitation):  # 강수량 크기 만큼 돌림
+    safe_area_count = 0  # 강수량 돌릴 때마다 안전영역 크기 0으로 초기화
 
-    visit_graph = [[False for _ in range(N)] for _ in range(N)]
+    visit_graph = [[False for _ in range(N)] for _ in range(N)]  # 방문 배열도 초기화
 
+    # BFS 시작
     for i in range(N):
         for j in range(N):
-            if graph[i][j] > p and not visit_graph[i][j]:
+            if graph[i][j] > p and not visit_graph[i][j]:  # 안전영역일 경우 and 방문 안했을 경우
                 bfs(graph, i, j, p)
-                safe_area_count += 1
-    safe_area.append(safe_area_count)
+                safe_area_count += 1  # 안전영역 집합 카운트
+    safe_area.append(safe_area_count)  # 안전영역 개수 집어 넣음
 
-print(max(safe_area))
+print(max(safe_area))  # 안전 영역 최대값 출력
