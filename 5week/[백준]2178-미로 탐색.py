5e770c6f8250cada5b37e598ac9da2eee0a09a46
YoonJoony <dbswns1101@naver.com>
2024-01-17 15:45:10 +0900

    2차 수정 -> 성공 bfs의 큐를 활용하고, 그래프에 경로마다 시작과의 거리를 카운트하여 저장한다.

--- 5week/[백준]2178-미로 탐색.py
+++ 5week/[백준]2178-미로 탐색.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
@@ -6,37 +8,27 @@ N, M = map(int, input().split())
 
 graph = [list(map(int, input().strip('\n'))) for _ in range(N)]
 
-visited = [[False for _ in range(M)] for _ in range(N)]
-
-dx = [1, -1]
-dy = [1, -1]
-result = []
-count = 1 # 거리 카운트
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-def dfs(y, x, graph):
-    visited[y][x] = True
-    global dx, dy, count
-    if y == N - 1 and x == M - 1:
-        visited[y][x] = False
-        result.append(count)
-
-    if graph[y][x] == 1:
-        # 상하좌우 1인 수 찾기위해 dx, dy값 활용
-        for i in range(2):
-            mx = x + dx[i] # 이동시킬 좌표
+def bfs(graph):
+    global cnt
+    graph[0][0] = 0
+    queue = deque([(0, 0)])
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            mx = x + dx[i]
             my = y + dy[i]
 
-            # 마지막 좌표가 아니면서 이동할 좌표가 범위를 벗어나지 않으면서 노드를 방문 안헀을 시
-            if (y != N - 1 or x != M - 1) and 0 <= mx < M and graph[y][mx] == 1 and not visited[y][mx]:
-                count += 1
-                dfs(y, mx, graph)
-            if (y != N - 1 or x != M - 1) and 0 <= my < N and graph[my][x] == 1 and not visited[my][x]:
-                count += 1
-                dfs(my, x, graph)
-    # 위 재귀함수 호출 후 재귀될 때 거리를 다시 원상 복구하여 1씩 줄인다.
-    count -= 1
+            if mx < 0 or mx >= N or my < 0 or my >= M:
+                continue
+
+            if graph[mx][my] == 1:
+                graph[mx][my] += graph[x][y]
+                queue.append((mx, my))
 
 
-dfs(0, 0, graph)
-print(min(result))
+bfs(graph)
+print((graph[N-1][M-1] + 1))
