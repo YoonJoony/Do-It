3209b7895fac4b3344a0786bd4758a86be952583
YoonJoony <dbswns1101@naver.com>
2024-01-18 11:44:09 +0900

    commit

--- 6week/[백준]1697-숨바꼭질.py
+++ 6week/[백준]1697-숨바꼭질.py
@@ -5,31 +5,48 @@ from collections import deque
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-graph = [0 for _ in range(math.ceil(K / 10) * 10)]
+graph = [0 for _ in range((math.ceil(K / 10) * 10)+1)]
 
-print(graph)
 d = [-1, 1, 2]
 
-
+print(graph)
 def bfs(graph):
     queue = deque([N])
     while queue:
         v = queue.popleft()
         for i in range(3):
-            if d[i] == 2:
-                md = v * d[i]
-            else:
-                md = v + d[i]
+            if round(K / 2)
+
+            if md < 0 or md >= len(graph):
+                continue
+
             graph[md] = graph[v] + 1
             queue.append(md)
-            if graph[md] == K:
+            print(queue)
+            if md == K:
                 queue.clear()
+
+while True:
+    if N <= round(K / 2):
+        N *= 2
+    elif N > round(K / 2) or N > K:
+        N -= 1
+ㅛ
 
 
 bfs(graph)
 print(graph)
+print(graph[K])
 
-# 33
+# 14 26 => 14 13 26
+# 14 24 => 14 13 12 24
+# 14 23 => 14 13 12 24 23
+# 14 22 => 14 13 12 11 22
+# 14 21 => 14 13 12 11 22 21
+# 14 20 => 14 13 12 11 10 20
+# 14 19 => 14 15 16 17 18 19
+# 24
+
 # 5 10 20 19 18 17
 # 5 10 9 18 17
 # 5 6 12 13 14 15 16 17
--- 6week/[백준]7569-토마토.py
+++ 6week/[백준]7569-토마토.py
@@ -1,3 +1,46 @@
 import sys
-import math
 from collections import deque
+
+input = sys.stdin.readline
+
+M, N, H = map(int, input().split())
+graph = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+visit_graph = [[[False for _ in range(M)] for _ in range(N)] for _ in range(H)]
+
+
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+def bfs(graph, x, y, z):
+    queue = deque([(x, y, z)])
+    visit_graph[x][y][z] = True
+
+    while queue:
+        x, y, z = queue.popleft()
+
+        for i in range(6):
+            mx = x + dx[i]
+            my = y + dy[i]
+            mz = z + dz[i]
+
+            if mx < 0 or mx >= H or my < 0 or my >= N or mz < 0 or mz >= M:
+                continue
+            if graph[mx][my][mz] == 0:
+                queue.append((mx, my, mz))
+                graph[mx][my][mz] = 1
+                visit_graph[x][y][z] = True
+
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if graph[i][j][k] == 1 and not visit_graph[i][j][k]:
+                bfs(graph, i, j, k)
+
+for i in range(H):
+    for j in range(N):
+        if graph[i][j].count(0) > 0:
+
+print(graph)
+
