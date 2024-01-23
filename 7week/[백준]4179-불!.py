2dde7b7128f17f09f5c0bd0e6336cb61746e9768
YoonJoony <dbswns1101@naver.com>
2024-01-23 13:07:17 +0900

    2차 작성 -> 성공 큐 두개 선언한 방식은 똑같음
    1차 작성이랑 차이점은 while을 돌 때마다 for문에서
    큐 자체에 좌표를 append 하는 것이 아니라
    
    for문 전체 1번 돌 때 탐색된 좌표를 큐에 저장하지 않고
    임시 저장하는 리스트(arr)에 저장함으로써 큐에 있던 좌표
    모두 덜어낸 다음 for문에서 나오면 그때 arr을 큐에 넣어준
    다. (while돌 때마다 arr 배열 다시 초기화)
    
    이렇게 두 큐 모두 위 방식으로 해주면
    지훈이위 이동과 불의 이동의 순서가 잘 맞아 떨어지게 되어
    올바른 답이 나올 수 있다.

--- 7week/[백준]4179-불!.py
+++ 7week/[백준]4179-불!.py
@@ -6,75 +6,70 @@ sys.setrecursionlimit(10 ** 6)
 R, C = map(int, input().split())
 
 graph = [list(str(input().rstrip('\n'))) for _ in range(R)]
-visited = [[False for _ in range(C)] for _ in range(R)]
 
 j_queue = deque([])
 f_queue = deque([])
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-x, y = 0, 0
-
 for i in range(R):
     for j in range(C):
         if graph[i][j] == 'J':
+            j_queue.append((i, j))
             graph[i][j] = 0
-            x = i
-            y = j
-
         if graph[i][j] == 'F':
             f_queue.append((i, j))
-
         if graph[i][j] == '.':
             graph[i][j] = 0
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 
 def bfs(graph):
-    global j_queue, f_queue, x, y
-
-    if x == 0 or x == R - 1 or y == 0 or y == C - 1:
-        return 0
-
-    j_queue.append((x, y))
+    # 지훈이 위치가 저장된 큐 or 불 위치 저장된 큐가 값이 있으면 계속 돔
     while j_queue or f_queue:
-        if j_queue:
+        j_arr = []
+        f_arr = []
+
+        for i in range(len(j_queue)):
             x, y = j_queue.popleft()
+            if graph[x][y] != 'F':
+                if (x == 0 or x == R - 1 or y == 0 or y == C - 1) and graph[x][y] != 'F':
+                    return graph[x][y]
 
-        for i in range(4):
-            mx = x + dx[i]
-            my = y + dy[i]
+                for i in range(4):
+                    mx = x + dx[i]
+                    my = y + dy[i]
+                    if mx < 0 or mx >= R or my < 0 or my >= C:
+                        continue
+                    if graph[mx][my] == 0:
+                        j_arr.append((mx, my))
+                        graph[mx][my] = graph[x][y] + 1
 
-            if mx < 0 or mx >= R or my < 0 or my >= C or graph[mx][my] != 0:
-                continue
+        for i in range(len(j_arr)):
+            j_queue.append(j_arr[i])
 
-            if graph[mx][my] == 0 and graph[x][y] != 'F':
-                j_queue.append((mx, my))
-                graph[mx][my] = graph[x][y] + 1
-
-            if (mx == 0 or mx == R - 1 or my == 0 or my == C - 1) and graph[x][y] != 'F':
-                return graph[mx][my]
-        if f_queue:
+        # 1턴 불 4방향 확산
+        for i in range(len(f_queue)):
             x, y = f_queue.popleft()
-            visited[x][y] = True
+            for i in range(4):
+                mx = x + dx[i]
+                my = y + dy[i]
 
-        for i in range(4):
-            mx = x + dx[i]
-            my = y + dy[i]
+                if mx < 0 or mx >= R or my < 0 or my >= C:
+                    continue
 
-            if mx < 0 or mx >= R or my < 0 or my >= C or graph[mx][my] == '#':
-                continue
+                if graph[mx][my] != '#' and graph[mx][my] != 'F':
+                    f_arr.append((mx, my))  # 확산된 불 좌표 저장
+                    graph[mx][my] = 'F'
+        # 확산된 불 좌표 큐에 저장
+        for i in range(len(f_arr)):
+            f_queue.append(f_arr[i])
 
-            if not visited[mx][my]:
-                f_queue.append((mx, my))
-
-            graph[mx][my] = 'F'
     return -1
 
 
 result = bfs(graph)
-
 if result == -1:
-    print('IMPOSSIBLE')
+    print("IMPOSSIBLE")
 else:
     print(result + 1)
