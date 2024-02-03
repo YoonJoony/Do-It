6bba5c9b2ef20439d76460950baddcd9724a0df8
YoonJoony <dbswns1101@naver.com>
2024-02-03 22:23:14 +0900

    4차 작성 -> 참고 고드 -> 원본배열 복사

--- 8week/[백준 골5]2447-별찍기 10.py
+++ 8week/[백준 골5]2447-별찍기 10.py
@@ -1,31 +1,25 @@
 import sys
+
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 
 def star_ten(size):
-    new_size = size // 3
-    if size == 3: # 3x3 크기 원본 배열 설정
-        graph[1] = ['*', ' ', '*']
-        graph[0][:3] = graph[2][:3] = ['*'] * 3
-        return
+    if size == 1:
+        return ['*']
 
-    star_ten(new_size)
+    Stars = star_ten(size // 3)
+    L = []
 
-    for i in range(0, size, new_size): # i, ㅓ 에 new_size를 더하면서 반복
-        for j in range(0, size, new_size):
-            if i != new_size or j != new_size:
-                for k in range(new_size):
-                    # new_ize 크기 만큼 원본배열 복사
-                    graph[i+k][j:j+new_size] = graph[k][:new_size]
-                    # new_size만큼 복사
+    # 원본 배열 생성
+    for S in Stars:
+        L.append(S * 3)
+    for S in Stars:
+        L.append(S + " " * (size // 3) + S)
+    for S in Stars:
+        L.append(S * 3)
+    return L # 원본 배열 리턴
 
 
 n = int(input())
-graph = [[" " for _ in range(n)] for _ in range(n)]
-
-star_ten(n)
-for i in range(n):
-    for j in range(n):
-        print(graph[i][j], end="")
-    print()
+print('\n'.join(star_ten(n)))
