e71e73fa7ac9d8896d290a27791fac7e28893124
YoonJoony <dbswns1101@naver.com>
2024-02-26 01:23:08 +0900

    2차 작성 -> dfs로 모든 경우 조합해서 성공

--- 11week/[백준 실버1]10819-차이를 최대로.py
+++ 11week/[백준 실버1]10819-차이를 최대로.py
@@ -2,31 +2,27 @@ from collections import deque
 
 N = int(input())
 lst = list(map(int, input().split()))
-lst.sort()
-
-flag = 0
+visited = [0] * N
 result = 0
-for i in range(N):
-    lst2 = deque(lst[:])
-    sm = 0
-    q = [lst2[(N-1) - i]]
-    lst2.remove(q[0])
 
-    for j in range(N):
-        if len(q) == N:
-            for k in range(N):
-                if k < N - 1:
-                    sm += abs(q[k] - q[k+1])
 
-            result = max(result, sm)
-            continue
+def dfs(n, arr):
+    global result
 
-        if flag == 0:
-            q.append(lst2.popleft())
-            flag = 1
+    if n == N:
+        sm = 0
+        for k in range(N):
+            if k < N - 1:
+                sm += abs(arr[k] - arr[k + 1])
+        result = max(result, sm)
+        return
 
-        else:
-            q.append(lst2.pop())
-            flag = 0
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            dfs(n + 1, arr + [lst[i]])
+            visited[i] = False
 
+
+dfs(0, [])
 print(result)
