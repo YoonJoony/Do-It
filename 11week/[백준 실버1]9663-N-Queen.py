8cd7140b4d34e64a4acabdcf7afd6d92c660b167
YoonJoony <dbswns1101@naver.com>
2024-02-26 16:50:38 +0900

    1차 작성 참고 -> 성공 (이해 힘듬)

--- 11week/[백준 실버1]9663-N-Queen.py
+++ 11week/[백준 실버1]9663-N-Queen.py
@@ -1,67 +1,22 @@
-import copy
+import sys
+input = sys.stdin.readline
+
+def dfs(n):
+    global ans
+    if n==N:
+        ans += 1
+        return
+
+    for j in range(N):
+        if v1[j] == v2[n + j] == v3[n - j] == 0:
+            v1[j] = v2[n + j] = v3[n - j] = 1
+            dfs(n+1)
+            v1[j] = v2[n + j] = v3[n - j] = 0
 
 N = int(input())
-
-graph = [[0 for _ in range(N)] for _ in range(N)]
-visited = [[False for _ in range(N)] for _ in range(N)]
-
-dx = [-1, -1, -1, 0, 1, 1, 1, 0]
-dy = [-1, 0, 1, 1, 1, 0, -1, -1]
-result = 0
-
-
-def queen_move(x, y, g):
-    graph2 = [arr[:] for arr in g]
-    graph2[x][y] = 2
-    cnt = 0
-    for i in range(8):
-        mx = x + dx[i]
-        my = y + dy[i]
-        while True:
-            if mx < 0 or mx >= N or my < 0 or my >= N:
-                break
-
-            if graph2[mx][my] == 2:
-                cnt -= 1
-                break
-
-            graph2[mx][my] = 1
-            mx += dx[i]
-            my += dy[i]
-
-        cnt += 1
-
-    if cnt == 8:
-        return graph2
-    else:
-        return 0
-
-
-def dfs(n, lst, g):
-    global result
-
-    if n == 8:
-        if len(lst) == 8:
-            result += 1
-        return
-    prev1, prev2 = -1, -1
-
-    for i in range(0, N):
-        for j in range(0, N):
-            if not visited[i][j] and g[i][j] < 1 and prev1 != i and prev2 != j:
-                visited[i][j] = True
-                let = queen_move(i, j, g)
-                prev1, prev2 = i, j
-
-                if let != 0:
-                    dfs(n + 1, lst + [[i, j]], let)
-                elif let == 0:
-                    dfs(n, lst, g)
-
-                visited[i][j] = False
-
-
-prev = [[-1, -1]]
-dfs(0, [], graph)
-
-print(result)
+ans = 0
+v1 = [0] * N
+v2 = [0] * (2*N)
+v3 = [0] * (2*N)
+dfs(0)
+print(ans)
