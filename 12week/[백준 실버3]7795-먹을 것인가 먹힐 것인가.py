f901e580e0367a2ac7e6d9a237ff72fab0319bf8
YoonJoony <dbswns1101@naver.com>
2024-03-04 16:17:34 +0900

    2차 작성 참고 -> 성공

--- 12week/[백준 실버3]7795-먹을 것인가 먹힐 것인가.py
+++ 12week/[백준 실버3]7795-먹을 것인가 먹힐 것인가.py
@@ -1,25 +1,27 @@
-import sys
-input = sys.stdin.readline
-# A의 가장 큰 수를 B와 비교
-# B의 가장 작은 수를 다음 A의 가장 큰 수와 비교
-# if 다음 A의 수가 B의 가장 작은 수보다 크다면 진행 else 노 진행
+# 이진/이분 탐색
+def binary_search(array, target):
+    # 시작, 끝
+    start, end = 0, len(array) - 1
+    while start <= end:
+        mid = (start + end) // 2  # 중간
+        if array[mid] < target:  # 타겟이 중간보다 같거나 클 경우
+            start = mid + 1  # 시작을 중간에서 + 1로 함.
+        else:  # 아닐경우 끝을 중간 -1로 함.
+            end = mid - 1
+    return start
 
-for _ in range(int(input())):
-    N, M = map(int, input().split())
-    A_lst = list(map(int, input().split()))
-    B_lst = list(map(int, input().split()))
-    min_B = B_lst[0]
-    sm = 0
-    cnt = 0
-    for i in range(len(A_lst)):
-        n = 0
-        if i != 0 and A_lst[i] < min_B:
-            continue
-        while n < len(B_lst):
-            if min_B > B_lst[n]:
-                min_B = B_lst[n]
-            if A_lst[i] > B_lst[n]:
-                cnt += 1
-            n += 1
 
-    print(cnt)
+def solve(A, B):
+    B.sort()
+    answer = 0
+    for a in A:
+        answer += binary_search(B, a)
+    return answer
+
+
+T = int(input())  # 테스트 케이스의 수
+for _ in range(T):
+    _, _ = map(int, input().split())  # A와 B의 크기(사용하지 않음)
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    print(solve(A, B))
