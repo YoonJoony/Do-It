444500ba5631e396e9aadb2c03535b4434b6bf79
YoonJoony <dbswns1101@naver.com>
2024-03-18 17:51:35 +0900

    2차 작성 -> 안보고 풀어봄

---
+++ 13week/[백준 실버1]1932-정수삼각형.py
@@ -0,0 +1,15 @@
+# 삼각형의 크기 입력 받기
+n = int(input())
+# 삼각형 데이터 입력 받기
+triangle = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(len(triangle[i])):
+        if (j-1) < 0:
+            triangle[i][j] += triangle[i-1][j]
+        elif (j-1) >= 0 and (j+1) < len(triangle[i]):
+            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+        else:
+            triangle[i][j] += triangle[i-1][j-1]
+
+print(max(triangle[n-1]))
